af4e20e205318bd8a204062384ce334eac1d98b2
lujiabin <lu10361750@163.com>
2018-12-19 15:25:15 +0800

    提交南天测试项目

---
+++ ssm-natian/src/main/java/com/jiabin/nanitan/login/action/LoginAction.java
@@ -0,0 +1,23 @@
+package com.jiabin.nanitan.login.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author Acer
+ *@description:登录action
+ *
+ *
+ *
+ */
+
+public class LoginAction{
+	
+/**
+	 * 
+	 */
+	
+public String execute() {
+	return "success";
+	
+}
+}
